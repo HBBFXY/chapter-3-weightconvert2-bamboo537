ea00337b7831131df1641f9c880e63d2f2471022
bamboo537 <1413242518@qq.com>
2025-09-18 22:29:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ print ("未来10年地球和月球体重变化:")
 for year in range (1,years +1):
     earth_weight=current_earth_weight+annual_gain*year
     moon_weight=earth_weight*moon_ratio
-    print (f"第｛year｝年:地球上体重{earth_weight:.2f}kg,月球上体重{moon_weight:.2f}kg")
+    print (f"第{year}年:地球上体重{earth_weight:.2f}kg,月球上体重{moon_weight:.2f}kg")
